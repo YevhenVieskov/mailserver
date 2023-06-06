613e8d3424a81e4db3e5a324dac54fb266d80be3
YevhenVieskov <evveskov@gmail.com>
2023-06-06 18:13:52 +0300

    userdata fixed

--- iac_dms/bootstrapdms.sh
+++ iac_dms/bootstrapdms.sh
@@ -3,8 +3,6 @@
 set -x # Print expanded commands to stdout
 #https://github.com/docker-mailserver/docker-mailserver
 
-#exec > >(tee /var/log/user-data.log|logger -t user-data -s 2>/dev/console) 2>&1
-#echo BEGIN
 
 #install git
 apt install -y git
@@ -77,7 +75,7 @@ ansible-galaxy install hmlkao.docker_mailserver  #not install
 #has no attribute 'stats'. 
 #second run skip failed task and run docker-mailserver
 
-chown -R ubuntu ${folder}/mailserver
+chgrp -R ubuntu:ubuntu ${folder}/mailserver
 
 ansible-playbook -u ubuntu ${folder}/mailserver/ansible/docker_mailserver.yml
 ansible-playbook -u ubuntu  ${folder}/mailserver/ansible/docker_mailserver.yml
@@ -105,34 +103,66 @@ aws route53 list-resource-record-sets --hosted-zone-id $hz_id
 
 
 # https://stackoverflow.com/questions/49228500/creating-route53-record-sets-via-shell-script
-aws route53 change-resource-record-sets \
-  --hosted-zone-id $hz_id \
-  --change-batch '
-  {
-    "Comment": "Testing creating a record set"
-   ,"Changes": [{
-      "Action"              : "CREATE"
-      ,"ResourceRecordSet"  : {
-        "Name"              : "mail._domainkey"
-        ,"Type"             : "TXT"
-        ,"TTL"              : 300
-        ,"ResourceRecords"  : [{
-            "Value"         : "'" $DKIM_SPLIT "'"
+#aws route53 change-resource-record-sets \
+#  --hosted-zone-id $hz_id \
+#  --change-batch '
+#  {
+#    "Comment": "Testing creating a record set",
+#   "Changes": [{
+#      "Action"              : "CREATE",
+#      "ResourceRecordSet"  : {
+#        "Name"              : "_domainkey",
+#        "Type"             : "TXT",
+#        "TTL"              : 300,
+#        "ResourceRecords"  : [{
+#            "Value"         : "'"$DKIM_SPLIT"'"
+#        }]
+#      }
+#    }]
+#  }
+#  '
+
+  cat << EOF > dkim.json
+{
+    "Comment": "Testing creating a record set",
+   "Changes": [{
+      "Action"              : "CREATE",
+      "ResourceRecordSet"  : {
+        "Name"              : "_domainkey",
+        "Type"             : "TXT",
+        "TTL"              : 300,
+        "ResourceRecords"  : [{
+            "Value"         : ${DKIM_SPLIT}
         }]
       }
     }]
   }
-  '
+EOF
 
-cd ~
+aws route53 change-resource-record-sets \
+  --hosted-zone-id $hz_id \
+  --change-batch file://dkim.json
 
-sudo groupadd docker || true 
-sudo usermod -aG docker $USER  || true 
+--change-batch file://dkim.json
 
-#echo END
-
-
-
+groupadd docker || true 
+usermod -aG docker $USER  || true 
+newgrp docker
 
 
 
+
+#{
+#            "Name": "_domainkey.mail.vieskov.com.",
+#            "Type": "TXT",
+#            "TTL": 300,
+#            "ResourceRecords": [
+#                {
+#                    "Value": "\"v=DKIM1; h=sha256; k=rsa; p=MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAt8O2tYOlDBFvxLTpzjynQ/kV6rzIEvTqPLO8Sgz9K5NIjeHEWsyGVbh6pZH0v9J7SGzxNQdowACp0JdFEfc7vWCNkJZ9KQMR66NJsZNsoL4msBAiwv+dgz3Dn0uM2RGhgKR6L5kiWdK3VDv7uHJIeJiZ\" \"4JEj8MRmEvmj10FShnuKo33TAWLKUrF2Qb/IgOtzRYgRvpRsDa2C3S6A8lDAOgsqqaldWGH38AFectDl8P6CyTJENMjLMBC0R5Y8qAUs0dNJ2bEq3n7VypkV0NCu0sNhmMrWVW3kcn1tymXiGTyjXzMd8AnrA69X7t/BqNXOLor0Jkdic8OB4BnDAHhOBQIDAQAB\""
+#                }
+#            ]
+#        }
+
+
+
+#DKIM_SPLIT="\"v=DKIM1; h=sha256; k=rsa; p=${key1}\" \"${key2}\""
